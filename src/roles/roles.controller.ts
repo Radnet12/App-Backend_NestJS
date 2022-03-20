e42b67c6748794159c61f367c47e83c6e6f03ae5
Sergey Vasylenko <sergeyvasylenko4@gmail.com>
2022-03-20 18:11:32 +0200

    added roles description for swagger

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,16 +1,25 @@
+import { Role } from "src/roles/roles.model";
+import { ApiResponse } from "@nestjs/swagger";
+import { ApiOperation } from "@nestjs/swagger";
+import { ApiTags } from "@nestjs/swagger";
 import { CreateRoleDto } from "./dto/createRole.dto";
 import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { RolesService } from "./roles.service";
 
+@ApiTags("RolesController")
 @Controller("roles")
 export class RolesController {
   constructor(private roleService: RolesService) {}
 
+  @ApiOperation({ summary: "Создание роли" })
+  @ApiResponse({ status: 200, type: Role })
   @Post()
   create(@Body() dto: CreateRoleDto) {
     return this.roleService.createRole(dto);
   }
 
+  @ApiOperation({ summary: "Получение роли по значению" })
+  @ApiResponse({ status: 200, type: [Role] })
   @Get("/:value")
   getByValue(@Param("value") value: string) {
     return this.roleService.getRoleByValue(value);
