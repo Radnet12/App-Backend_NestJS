45895b03ef7b1f5368110d1f45317ccb802f08bd
Sergey Vasylenko <sergeyvasylenko4@gmail.com>
2022-03-20 13:34:27 +0200

    added first model

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
+
   await app.listen(PORT, () =>
     console.log(`Server started on the port: ${PORT}`)
   );
---
+++ src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { CreateUserDto } from "./dto/createUser.dto";
+import { UsersService } from "./users.service";
+
+@Controller("users")
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this.usersService.createUser(userDto);
+  }
+
+  @Get()
+  getAll() {
+    return this.usersService.getAllUsers();
+  }
+}
